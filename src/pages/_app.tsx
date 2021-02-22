54bd975832babcdbff2576ee13c7ca769f01c479
Andrew Keturi <andrew.keturi@mirpartners.com>
2021-02-21 17:58:20 -0800

    Hook in urql

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,28 @@
 import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react'
-
+import React from 'react'
+import { Provider, createClient } from 'urql'
 import theme from '../theme'
+
+const client = createClient({
+  url: 'http://localhost:4000/graphql',
+  fetchOptions: {
+    credentials: 'include',
+  }
+});
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider resetCSS theme={theme}>
-      <ColorModeProvider
-        options={{
-          useSystemColorMode: true,
-        }}
-      >
-        <Component {...pageProps} />
-      </ColorModeProvider>
-    </ChakraProvider>
+    <Provider value={client}>
+      <ChakraProvider resetCSS theme={theme}>
+        <ColorModeProvider
+          options={{
+            useSystemColorMode: false,
+          }}
+        >
+          <Component {...pageProps} />
+        </ColorModeProvider>
+      </ChakraProvider>
+    </Provider>
   )
 }
 
--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -3,15 +3,33 @@ import { Form, Formik } from 'formik';
 import { FormControl, FormLabel, Input, FormErrorMessage, Button } from '@chakra-ui/react';
 import { Wrapper } from '../components/Wrapper';
 import { InputField } from '../components/InputField';
+import { useMutation } from "urql";
 
 interface RegisterProps {
 
 }
 
+const REGISTER_MUTATION = `
+mutation Register($username:String!, $password:String!){
+    register(options:{username: $username, password: $password}){
+      user {
+          id
+          username
+      }
+      errors {
+        field
+        message
+      }
+    }
+  }
+`
+
 export const Register: React.FC<RegisterProps> = () => {
+
+    const [,register] = useMutation(REGISTER_MUTATION);
     return (
         <Wrapper variant='small'>
-            <Formik initialValues={{ username: ""}} onSubmit={() => console.log("AA")}>
+            <Formik initialValues={{ username: "", password: ""}} onSubmit={(values) => register(values)}>
                 {({ isSubmitting} ) => (
                     <Form>
                         <InputField name='username' placeholder='username' label='Username'/>

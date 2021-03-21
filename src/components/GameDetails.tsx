66538dafbb1506c84cb8fd16148121fc4838233a
Andrew Keturi <andrew.keturi@mirpartners.com>
2021-03-20 22:55:55 -0700

    Add mobile layout to game page

--- src/components/GameDetails.tsx
+++ src/components/GameDetails.tsx
@@ -2,37 +2,56 @@ import { EditIcon } from "@chakra-ui/icons";
 import {
   Badge,
   Box,
-  Button,
   Flex,
   Heading,
   IconButton,
   Image,
   Link,
-  Spacer,
   Text,
+  useBreakpoint,
+  useBreakpointValue,
 } from "@chakra-ui/react";
+import NextLink from "next/link";
 import { useRouter } from "next/router";
 import * as React from "react";
 import { RegularGameFragment, useMeQuery } from "../generated/graphql";
+import { FALLBACK_BANNER, FALLBACK_THUMBNAIL } from "../utils/constants";
 import { CommentList } from "./CommentList";
 import { DownloadList } from "./DownloadList";
 import { FavoriteButton } from "./FavoriteButton";
 import { Layout } from "./Layout";
 import { ScreenshotList } from "./ScreenshotList";
-import { TextSection } from "./TextSection";
-import NextLink from "next/link";
 import { TextChunk } from "./TextChunk";
-import { FALLBACK_BANNER, FALLBACK_THUMBNAIL } from "../utils/constants";
+import { TextSection } from "./TextSection";
 
 interface GameDetailsProps {
   game: RegularGameFragment;
 }
+
+const GameThumbnail: React.FC<GameDetailsProps> = ({ game }) => {
+  const height = useBreakpointValue({ base: "150px", sm: "200px" });
+  return (
+    <Box zIndex="1" flex="1" h={height} mt="-100px" mx="20px">
+      <Image
+        src={game.thumbnail || ""}
+        fallbackSrc={FALLBACK_THUMBNAIL}
+        objectFit="cover"
+        height="100%"
+        width="100%"
+        mb="20px"
+      />
+      <FavoriteButton game={game} preset="lg" />
+    </Box>
+  );
+};
 
 export const GameDetails: React.FC<GameDetailsProps> = ({ game }) => {
   const { data } = useMeQuery();
   const router = useRouter();
 
   const canEdit = data?.me?.isAdmin || data?.me?.id === game.submitter.id;
+
+  const vertical = useBreakpointValue({ base: true, sm: false });
   const header = (
     <Box
       height="300px"
@@ -62,46 +81,46 @@ export const GameDetails: React.FC<GameDetailsProps> = ({ game }) => {
     <>
       <Layout header={header} title={`${game.title}, Classic RPG Maker Game`}>
         <Flex>
-          <Box zIndex="1" flex="1" h="200px" mt="-100px" mx="20px">
-            <Image
-              src={game.thumbnail || ""}
-              fallbackSrc={FALLBACK_THUMBNAIL}
-              objectFit="cover"
-              height="100%"
-              mb="20px"
-            />
-            <FavoriteButton game={game} preset="lg" />
-          </Box>
+          {!vertical && <GameThumbnail game={game} />}
           <Box flex="3">
-            <Box position="absolute" top="200px">
-              <Heading color="blue.50" as="h1" size="lg" mb="5px">
-                {game.title}
-                {canEdit && (
-                  <IconButton
-                    ml={1}
-                    size="sm"
-                    variant="ghost"
-                    color="blue.50"
-                    aria-label="Edit Game"
-                    onClick={() => router.push(`/games/edit/${game.slug}`)}
-                    icon={<EditIcon />}
-                  />
+            <Box position="absolute" top="200px" width="100%">
+              <Flex>
+                <Box>
+                  <Heading color="blue.50" as="h1" size="lg" mb="5px">
+                    {game.title}
+                    {canEdit && (
+                      <IconButton
+                        ml={1}
+                        size="sm"
+                        variant="ghost"
+                        color="blue.50"
+                        aria-label="Edit Game"
+                        onClick={() => router.push(`/games/edit/${game.slug}`)}
+                        icon={<EditIcon />}
+                      />
+                    )}
+                  </Heading>
+                  <Heading color="blue.100" as="h2" size="md" mb="5px">
+                    {game.author || "Unknown Author"}
+                  </Heading>
+                  <Heading color="blue.100" as="h2" size="md" mb="15px">
+                    {game.year || "2000"}
+                  </Heading>
+                  {game?.tags?.split(",").map(
+                    (tag) =>
+                      tag.length > 0 && (
+                        <Badge key={tag} mb="15px" mr="5px">
+                          {tag}
+                        </Badge>
+                      )
+                  )}
+                </Box>
+                {vertical && (
+                  <Box w="200px" right={5} top={-7} position="absolute">
+                    <GameThumbnail game={game} />
+                  </Box>
                 )}
-              </Heading>
-              <Heading color="blue.100" as="h2" size="md" mb="5px">
-                {game.author || "Unknown Author"}
-              </Heading>
-              <Heading color="blue.100" as="h2" size="md" mb="15px">
-                {game.year || "2000"}
-              </Heading>
-              {game?.tags?.split(",").map(
-                (tag) =>
-                  tag.length > 0 && (
-                    <Badge key={tag} mb="15px" mr="5px">
-                      {tag}
-                    </Badge>
-                  )
-              )}
+              </Flex>
             </Box>
             <TextSection heading="About">
               <TextChunk text={game.longDescription || "No description"} />
